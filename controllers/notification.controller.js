dd316ec3a4b6ce25e855c5e5f0d5a56c451e3020
Hamdan KT <hamdankt@onlinesafe.in>
2025-03-30 00:39:20 +0530

    upated web push config

--- controllers/notification.controller.js
+++ controllers/notification.controller.js
@@ -3,6 +3,8 @@ import { ApiError } from "../utils/ApiError.js";
 import { ApiSuccess } from "../utils/ApiSuccess.js";
 import webpush from "web-push";
 import NotiSubscription from "../Models/notisubscription.model.js";
+import dotenv from "dotenv";
+dotenv.config();
 
 export const subscribeNotification = asyncHandler(async (req, res, next) => {
 	const { subscription } = req.body;
@@ -27,15 +29,18 @@ export const sendNotification = asyncHandler(async (req, res, next) => {
 	const payload = JSON.stringify({ title: "instogram", body: message });
 
 	// // web-push config
-	// webpush.setVapidDetails(
-	// 	"mailto:hamdankz786@gmail.com",
-	// 	process.env.PUBLIC_VAPID_KEY,
-	// 	process.env.PRIVATE_VAPID_KEY
-	// );
+	webpush.setVapidDetails(
+		"mailto:hamdankz786@gmail.com",
+		process.env.PUBLIC_VAPID_KEY,
+		process.env.PRIVATE_VAPID_KEY
+	);
 	//push notification using web-push
 	webpush
 		.sendNotification(userSubscription.subscription, payload)
-		.then(() => ApiSuccess(res, "Notification sent successfully.", {}))
+		.then((response) => {
+			console.log(response);
+			return ApiSuccess(res, "Notification sent successfully.", {});
+		})
 		.catch((error) => {
 			console.error("Push Notification Error:", error);
 			return next(new ApiError(500, "Failed to send push notification."));
